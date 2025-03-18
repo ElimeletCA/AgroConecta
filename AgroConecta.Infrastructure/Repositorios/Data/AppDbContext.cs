b804fe354f4a0f9da9218e5cc8b6d3ed60178d46
ElimeletCA <elimelet.dev@gmail.com>
2025-03-18 09:38:33 -0400

    Correcion de efcore

--- AgroConecta.Infrastructure/Repositorios/Data/AppDbContext.cs
+++ AgroConecta.Infrastructure/Repositorios/Data/AppDbContext.cs
@@ -16,19 +16,7 @@ public class AppDbContext : IdentityDbContext<Usuario>
     
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-        try
-        {
-            var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
-            if (databaseCreator != null)
-            {
-                if (!databaseCreator.CanConnect()) databaseCreator.Create();
-                if (!databaseCreator.HasTables()) databaseCreator.CreateTables();
-            }
-        }
-        catch (System.Exception e)
-        {
-            Debug.WriteLine(e.Message);
-        }
+
     }
     public DbSet<SystemLog> SystemLogs { get; set; }
     public DbSet<Archivo> Archivos { get; set; }
