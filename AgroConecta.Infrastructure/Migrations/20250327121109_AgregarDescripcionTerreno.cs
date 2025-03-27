c117fb100abf30ae87df23cc0604ae5fac93246c
ElimeletCA <elimelet.dev@gmail.com>
2025-03-27 17:09:37 -0400

    .

---
+++ AgroConecta.Infrastructure/Migrations/20250327121109_AgregarDescripcionTerreno.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AgroConecta.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarDescripcionTerreno : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Perfil",
+                keyColumn: "Id",
+                keyValue: "006da7fd-3c10-4b70-90c2-7380c3e5febf");
+
+            migrationBuilder.DeleteData(
+                table: "Perfil",
+                keyColumn: "Id",
+                keyValue: "5286e36d-7418-4940-997b-0880959a2483");
+
+            migrationBuilder.DeleteData(
+                table: "Perfil",
+                keyColumn: "Id",
+                keyValue: "cd65bea9-e37d-4672-a7f3-115e1b911eaf");
+
+            migrationBuilder.DropColumn(
+                name: "RegistroActivo",
+                table: "Proyecto");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comentario",
+                table: "Terreno",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Descripción",
+                table: "Terreno",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comentario",
+                table: "Proyecto",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comentario",
+                table: "Arrendamiento",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Descripción",
+                table: "Arrendamiento",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "Perfil",
+                columns: new[] { "Id", "DeletedTimeUtc", "DescripcionPerfil", "IsDeleted", "LastUpdateUtc", "NombrePerfil" },
+                values: new object[,]
+                {
+                    { "5423b4eb-84f3-4521-9330-c7a70e8f8727", null, "Agricultor", false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Agricultor" },
+                    { "5d81051d-27c5-44aa-bd98-06d4f6d3e958", null, "Inversionista", false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Inversionista" },
+                    { "713c5c63-9aed-4226-8d71-ad5ea18db88d", null, "Propietario", false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Propietario" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Perfil",
+                keyColumn: "Id",
+                keyValue: "5423b4eb-84f3-4521-9330-c7a70e8f8727");
+
+            migrationBuilder.DeleteData(
+                table: "Perfil",
+                keyColumn: "Id",
+                keyValue: "5d81051d-27c5-44aa-bd98-06d4f6d3e958");
+
+            migrationBuilder.DeleteData(
+                table: "Perfil",
+                keyColumn: "Id",
+                keyValue: "713c5c63-9aed-4226-8d71-ad5ea18db88d");
+
+            migrationBuilder.DropColumn(
+                name: "Descripción",
+                table: "Terreno");
+
+            migrationBuilder.DropColumn(
+                name: "Descripción",
+                table: "Arrendamiento");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comentario",
+                table: "Terreno",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comentario",
+                table: "Proyecto",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "RegistroActivo",
+                table: "Proyecto",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comentario",
+                table: "Arrendamiento",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Perfil",
+                columns: new[] { "Id", "DeletedTimeUtc", "DescripcionPerfil", "IsDeleted", "LastUpdateUtc", "NombrePerfil" },
+                values: new object[,]
+                {
+                    { "006da7fd-3c10-4b70-90c2-7380c3e5febf", null, "Propietario", false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Propietario" },
+                    { "5286e36d-7418-4940-997b-0880959a2483", null, "Inversionista", false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Inversionista" },
+                    { "cd65bea9-e37d-4672-a7f3-115e1b911eaf", null, "Agricultor", false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Agricultor" }
+                });
+        }
+    }
+}
---
+++ AgroConecta.Infrastructure/Migrations/20250327131955_CorreccionDescripcionTerreno.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AgroConecta.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorreccionDescripcionTerreno : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Perfil",
+                keyColumn: "Id",
+                keyValue: "5423b4eb-84f3-4521-9330-c7a70e8f8727");
+
+            migrationBuilder.DeleteData(
+                table: "Perfil",
+                keyColumn: "Id",
+                keyValue: "5d81051d-27c5-44aa-bd98-06d4f6d3e958");
+
+            migrationBuilder.DeleteData(
+                table: "Perfil",
+                keyColumn: "Id",
+                keyValue: "713c5c63-9aed-4226-8d71-ad5ea18db88d");
+
+            migrationBuilder.RenameColumn(
+                name: "Descripción",
+                table: "Terreno",
+                newName: "Descripcion");
+
+            migrationBuilder.InsertData(
+                table: "Perfil",
+                columns: new[] { "Id", "DeletedTimeUtc", "DescripcionPerfil", "IsDeleted", "LastUpdateUtc", "NombrePerfil" },
+                values: new object[,]
+                {
+                    { "6e6814a0-87a7-491e-ae54-5090bda9be18", null, "Inversionista", false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Inversionista" },
+                    { "b75acc40-3491-45cc-b80e-58e9b559cdd7", null, "Propietario", false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Propietario" },
+                    { "f80e11d8-e9d1-4b7f-b106-a49e0173ab58", null, "Agricultor", false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Agricultor" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Perfil",
+                keyColumn: "Id",
+                keyValue: "6e6814a0-87a7-491e-ae54-5090bda9be18");
+
+            migrationBuilder.DeleteData(
+                table: "Perfil",
+                keyColumn: "Id",
+                keyValue: "b75acc40-3491-45cc-b80e-58e9b559cdd7");
+
+            migrationBuilder.DeleteData(
+                table: "Perfil",
+                keyColumn: "Id",
+                keyValue: "f80e11d8-e9d1-4b7f-b106-a49e0173ab58");
+
+            migrationBuilder.RenameColumn(
+                name: "Descripcion",
+                table: "Terreno",
+                newName: "Descripción");
+
+            migrationBuilder.InsertData(
+                table: "Perfil",
+                columns: new[] { "Id", "DeletedTimeUtc", "DescripcionPerfil", "IsDeleted", "LastUpdateUtc", "NombrePerfil" },
+                values: new object[,]
+                {
+                    { "5423b4eb-84f3-4521-9330-c7a70e8f8727", null, "Agricultor", false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Agricultor" },
+                    { "5d81051d-27c5-44aa-bd98-06d4f6d3e958", null, "Inversionista", false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Inversionista" },
+                    { "713c5c63-9aed-4226-8d71-ad5ea18db88d", null, "Propietario", false, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Propietario" }
+                });
+        }
+    }
+}
